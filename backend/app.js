3d7d3d6866a602812ee241b6d322fb4eb005e781
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-10-05 15:14:35 -0300

    server using express config

--- backend/app.js
+++ backend/app.js
@@ -4,3 +4,14 @@ const app = express();
 
 app.use;
 
+app.use ((_req, _res, next) => {
+    console.log( 'Firt middleware' );
+    next();
+});
+
+app.use ((_req, res, _next) => {
+    console.log( 'Firt middleware' );
+    res.send( 'Hello from express' );
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const http = require ('http');
+const app = require ('./backend/app.js');
+const port = process.env.PORT || 3000;
 
 /* req = request 
    res = response
 */
+const server = http.createServer(app);
 
-const server = http.createServer((req, res) => {
-    res.end('This is my first response');
-});
-
-server.listen(process.env.PORT || 3000);
+server.listen(port);
