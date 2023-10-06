ed9b8427c398fd90014a1e52728f91374086c7b9
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-10-06 16:14:39 -0300

    Update posts.service.ts

--- front/app/src/app/posts/posts.service.ts
+++ front/app/src/app/posts/posts.service.ts
@@ -12,7 +12,12 @@ export class PostsService {
   constructor( private http: HttpClient ) {}
 
   getPosts() {
-    return [...this.posts];
+    this.http.get<{ message: string, posts: [] }>('http://localhost:3000/api/posts')
+
+      .subscribe( (postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdateListener() {
@@ -20,7 +25,10 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = {title: title, content: content};
+    const post: Post = {
+      title: title, content: content,
+      id: ''
+    };
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
