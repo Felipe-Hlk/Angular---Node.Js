e58d58b0299529a4235bc16a8fccbd3f7a16fc8a
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-10-06 16:14:32 -0300

    Update api_app.js

--- backend/api_app.js
+++ backend/api_app.js
@@ -2,7 +2,21 @@ const express = require('express');
 
 const api_app = express();
 
-api_app.use ("/api/posts", (req, res, next) => {
+api_app.use((_req, res, next) => {
+  
+  res.setHeader("Access-Control-Allow-Origin", "*"); //cabeçalho compreendido pelo navegador
+  res.setHeader(
+    "Access-Control-Allow-Header",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Header",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+api_app.use ("/api/posts", (_req, res, _next) => {
     const posts = [
         {
             id: "001", 
